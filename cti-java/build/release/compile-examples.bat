6ec6041d2abec0d407d73f31bc184bac24c4bbae
MIYABE Tatsuhiko <tatsuhiko@miya.be>
2025-05-21 18:40:58 +0900

    remove cti-java/build

--- cti-java/build/release/compile-examples.bat
+++
@@ -1,7 +0,0 @@
-rd /s /q classes examples\webapp\WEB-INF\lib examples\webapp\WEB-INF\classes
-mkdir classes
-javac -source 1.8 -target 1.8 -classpath cti-driver-2.2.2.jar;lib/javax.servlet-api-4.0.1.jar;lib/jakarta.servlet-api-5.0.0.jar -encoding UTF-8 -d classes examples\src\jp\cssj\cti2\examples\*.java
-mkdir examples\webapp\WEB-INF\lib
-mkdir examples\webapp\WEB-INF\classes
-copy cti-driver-2.2.2.jar examples\webapp\WEB-INF\lib
-xcopy /e /c /y classes examples\webapp\WEB-INF\classes
